f44339d6962f9107a119afa44783d8fa3313a4f1
tona0516 <tona.meem@gmail.com>
2015-06-04 11:32:09 +0900

    upload

--- src/com/tona/cursorbrowser/GeneralPref.java
+++ src/com/tona/cursorbrowser/GeneralPref.java
@@ -7,16 +7,24 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class GeneralPref extends PreferenceActivity {
 	/** Called when the activity is first created. */
 	private static SharedPreferences sp;
 	private static EditTextPreference home;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.general_pref);
+		loadAdvertisements();
 		sp = PreferenceManager.getDefaultSharedPreferences(this);
 		home = (EditTextPreference) findPreference("homepage");
 		home.setText(sp.getString("homepage", MainActivity.DEFAULT_HOME));
@@ -32,5 +40,21 @@ public class GeneralPref extends PreferenceActivity {
 				return false;
 			}
 		});
+	}
+
+	private void loadAdvertisements() {
+		final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+		AdView adView = new AdView(this);
+		adView.setAdUnitId("ca-app-pub-4176998183155624/6476838791"); // 注1
+		adView.setAdSize(AdSize.BANNER);
+		RelativeLayout layout_ad = new RelativeLayout(this);
+		RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(WC, WC);
+		param.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 1);
+		param.addRule(RelativeLayout.CENTER_HORIZONTAL, 2);
+		layout_ad.addView(adView, param);
+		addContentView(layout_ad, param);
+
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
 	}
 }
--- src/com/tona/cursorbrowser/Pref.java
+++ src/com/tona/cursorbrowser/Pref.java
@@ -1,7 +1,13 @@
 package com.tona.cursorbrowser;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class Pref extends PreferenceActivity {
 	/** Called when the activity is first created. */
@@ -10,5 +16,22 @@ public class Pref extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.pref);
+		loadAdvertisements();
+	}
+
+	private void loadAdvertisements() {
+		final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+		AdView adView = new AdView(this);
+		adView.setAdUnitId("ca-app-pub-4176998183155624/6476838791"); // 注1
+		adView.setAdSize(AdSize.BANNER);
+		RelativeLayout layout_ad = new RelativeLayout(this);
+		RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(WC, WC);
+		param.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 1);
+		param.addRule(RelativeLayout.CENTER_HORIZONTAL, 2);
+		layout_ad.addView(adView, param);
+		addContentView(layout_ad, param);
+
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
 	}
 }
